5ea83bacc6627b94a5f745aa687f6bdcb8b45841
AndrewGolov <telegram @andre_g_life>
2026-06-04 22:31:35 +0300

    install redux package

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,28 +1,19 @@
 import { useState } from 'react';
 import { GameLayout } from './components/GameLayout/GameLayout';
-
-const WIN_PATTERNS = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8], // Варианты побед по горизонтали
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8], // Варианты побед по вертикали
-	[0, 4, 8],
-	[2, 4, 6], // Варианты побед по диагонали
-];
+import { createField } from './components/utils/createField';
+import { WIN_PATTERNS } from './components/constants/WIN_PATTERNS';
 
 export function Game() {
 	const [currentPlayer, setCurrentPlayer] = useState('X');
 	const [isGameEnded, setIsGameEnded] = useState(false);
 	const [isDraw, setIsDraw] = useState(false);
-	const [field, setField] = useState(['', '', '', '', '', '', '', '', '']);
+	const [field, setField] = useState(createField());
 
 	const checkForWin = (fieldGame, pattern, player) =>
 		pattern.some((pArr) => pArr.every((index) => fieldGame[index] === player));
 
 	const restartGame = () => {
-		setField(['', '', '', '', '', '', '', '', '']);
+		setField(createField());
 		setIsGameEnded(false);
 		setIsDraw(false);
 		setCurrentPlayer('X');
